3e3300987a8b722819a0bf15c3833033eb641dfd
Aman <av36232@gmail.com>
2021-05-09 22:02:55 +0530

    First Demo Commit containing prebuilt demo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_shopping_app/Constants/Constants.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -36,7 +37,146 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+        child: Scaffold(
+      body: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                flex: 6,
+                child: Container(
+                    color: Colors.black,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: MediaQuery.of(context).size.width * 0.3,
+                          horizontal:
+                              MediaQuery.of(context).size.width * 0.007),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        )),
+                        shadowColor: Colors.pinkAccent,
+                        elevation: 8,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 8, vertical: 10),
+                                  child: Text(
+                                    "Log In",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.07,
+                                    ),
+                                  ),
+                                )),
+                            Expanded(
+                                flex: 1,
+                                child: Text(
+                                  "With",
+                                  style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.width *
+                                            0.06,
+                                  ),
+                                )),
+                            Divider(
+                              thickness: 2,
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  IconButton(
+                                      icon: FaIcon(FontAwesomeIcons.twitter),
+                                      iconSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.15,
+                                      color: Colors.blue,
+                                      splashRadius: 1,
+                                      onPressed: () {
+                                        print("twitter");
+                                      }),
+                                  Text(
+                                    "Twitter",
+                                    style: TextStyle(fontSize: 20),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2,
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  IconButton(
+                                      icon: FaIcon(FontAwesomeIcons.facebook),
+                                      iconSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.15,
+                                      color: Colors.blue,
+                                      splashRadius: 1,
+                                      onPressed: () {
+                                        print("facebook");
+                                      }),
+                                  Text("Facebook",
+                                      style: TextStyle(fontSize: 20))
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2,
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  IconButton(
+                                      icon: FaIcon(FontAwesomeIcons.google),
+                                      iconSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.15,
+                                      color: Colors.red,
+                                      splashRadius: 1,
+                                      onPressed: () {
+                                        print("Google");
+                                      }),
+                                  Text("Google", style: TextStyle(fontSize: 20))
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ))),
+            Expanded(
+                flex: 14,
+                child: Container(
+                  color: Colors.black,
+                  child: Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                        Radius.circular(40),
+                      )),
+                      color: Colors.white,
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    ));
   }
 }
 
